9ddeffc92bbebbde0d01d79313a23b7a1dd84351
Mahdius <silvamod@gmail.com>
2022-11-27 00:52:12 +0200

    Fixed centering texts css, started on scrollable div

--- src/Content.jsx
+++ src/Content.jsx
@@ -3,23 +3,28 @@ import { useState,useEffect } from 'react'
 
 export default function Content(props) {
     var favourites = JSON.parse(localStorage.getItem('favourites'));
-    const [favouriteFlag, setfavouriteFlag] = useState('Add to favourites')
-
+    const [FavouriteMessage, setFavouriteMessage] = useState('Add To Favourites')
+    const notFavourited = false;
+    const Favourited = true;
 
     function saveFavourites(){
         localStorage.setItem('favourites', JSON.stringify(favourites));
     }
 
     function AddToFavourites(index){ 
+     
         if(!favourites){
             favourites = [index]
+            setFavourite(Favourited)
         }
         else {
             if(!(favourites.find(i => i == index) + 1)){
                 favourites.push(index)
+                setFavourite(Favourited)
             } 
             else{
                 favourites = favourites.filter(i => i != index)
+                setFavourite(notFavourited)
             }
             
         }
@@ -27,15 +32,23 @@ export default function Content(props) {
         
 
     }
+    function setFavourite(favBool){
+        console.log("asdasd")
+        if(favBool){
+            setFavouriteMessage('Favourite!')
+        } else {
+            setFavouriteMessage('Add to favourites')
+        }
+    }
 
     useEffect(() => {
-      if( (favourites.find(i => i == props.curIndex) + 1) ){
-            setfavouriteFlag('Favourite!')
+      if( favourites && (favourites.find(i => i == props.curIndex) + 1) ){
+        setFavouriteMessage('Favourite!')
       } else {
-            setfavouriteFlag('Add to favourites')
+        setFavouriteMessage('Add to favourites')
       }
-      console.log(favouriteFlag)
-    }, [favourites])
+ 
+    }, [props.curIndex])
     
 
     if(props.data)
@@ -43,7 +56,18 @@ export default function Content(props) {
     <>
         <h1 style={{fontSize:25,fontWeight:'bold'}}>{props.data.results[props.curIndex].title}</h1>
         <h2>{props.data.results[props.curIndex].opening_crawl}</h2>
-        <div onClick={() => {AddToFavourites(props.curIndex)}}>{favouriteFlag}</div>
+        <div>Release Date: {props.data.results[props.curIndex].release_date}</div>
+        <div>Director: {props.data.results[props.curIndex].director}</div>
+        <div>Producer: {props.data.results[props.curIndex].producer}</div>
+        <br/>
+        <div onClick={() => {AddToFavourites(props.curIndex)}}>{FavouriteMessage}</div>
+        <br/><br/><br/><br/>
+        <div className='ScrollableDiv'>
+
+    <div>adwqdq</div>  <br/><div>adwqdq</div>  <div>adwqdq</div>  <div>adwqdq</div>  <div>adwqdq</div>  <div>adwqdq</div>  
+    <div>adwqdq</div>  <div>adwqdq</div>  <div>adwqdq</div>  <div>adwqdq</div>  <div>adwqdq</div>  <div>adwqdq</div>  <div>adwqdq</div>
+
+        </div>
     </>
   ) 
   else return(<></>)
